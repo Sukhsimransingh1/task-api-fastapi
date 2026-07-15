68e3bc13ec849e8e30e3a2e42450b839d6bed669
Sukhsimran Singh <sukhsimransingh304@gmail.com>
2026-07-15 19:48:59 +0530

    Stage 3: create with validation

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel
 
 app = FastAPI(
     title="Task API",
     description="Simple CRUD API using FastAPI",
     version="1.0"
 )
+
+# Request Model
+class TaskCreate(BaseModel):
+    title: str
+
 
 # In-memory Database
 tasks = [
@@ -25,16 +31,16 @@ tasks = [
     }
 ]
 
+
 # Root Endpoint
 @app.get("/", summary="API Information")
 def root():
     return {
         "name": "Task API",
         "version": "1.0",
-        "endpoints": [
-            "/tasks"
-        ]
+        "endpoints": ["/tasks"]
     }
+
 
 # Health Check
 @app.get("/health", summary="Health Check")
@@ -43,10 +49,12 @@ def health():
         "status": "ok"
     }
 
+
 # Get All Tasks
 @app.get("/tasks", summary="Get All Tasks")
 def get_tasks():
     return tasks
+
 
 # Get Task By ID
 @app.get("/tasks/{task_id}", summary="Get Task By ID")
@@ -60,3 +68,30 @@ def get_task(task_id: int):
         status_code=404,
         detail=f"Task {task_id} not found"
     )
+
+
+# Create New Task
+@app.post(
+    "/tasks",
+    summary="Create New Task",
+    status_code=status.HTTP_201_CREATED
+)
+def create_task(task: TaskCreate):
+
+    title = task.title.strip()
+
+    if title == "":
+        raise HTTPException(
+            status_code=400,
+            detail="Title cannot be empty"
+        )
+
+    new_task = {
+        "id": len(tasks) + 1,
+        "title": title,
+        "done": False
+    }
+
+    tasks.append(new_task)
+
+    return new_task
